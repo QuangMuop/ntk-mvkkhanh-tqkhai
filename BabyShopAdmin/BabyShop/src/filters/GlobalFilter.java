2c5586699d1fe0125e55358a7d774fd5eec5c2fc
mvkkhanh <mvkkhanh@gmail.com>
2012-12-29 03:02:36 +0000

    - Sửa BabyShopAdmin filter - Từ h dùng từ BabyShop là admin - Còn BabyShopUser là user nha, tại hồi kì đặt tên thiếu Admin ngu vãi :(

--- BabyShopAdmin/BabyShop/src/filters/GlobalFilter.java
+++ BabyShopAdmin/BabyShop/src/filters/GlobalFilter.java
@@ -1,7 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package filters;
 
+import dao.BinhLuanDAO;
+import dao.LoaiDoChoiDAO;
+import dao.NhaSanXuatDAO;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.Filter;
@@ -10,62 +21,252 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import pojos.BinhLuan;
+import pojos.LoaiDoChoi;
+import pojos.NhaSanXuat;
 
 /**
- * Servlet Filter implementation class GlobalFilter
+ * 
+ * @author TrongKhoa
  */
-@WebFilter("/GlobalFilter")
 public class GlobalFilter implements Filter
 {
 
-    /**
-     * Default constructor.
-     */
+    private static final boolean debug = true;
+    // The filter configuration object we are associated with. If
+    // this value is null, this filter instance is not currently
+    // configured.
+    private FilterConfig filterConfig = null;
+
     public GlobalFilter()
     {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see Filter#destroy()
-     */
-    public void destroy()
+    private void doBeforeProcessing(ServletRequest request,
+            ServletResponse response) throws IOException, ServletException
     {
-        // TODO Auto-generated method stub
-    }
-
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException
-    {
-        // TODO Auto-generated method stub
-        // place your code here
+        if (debug)
+        {
+            log("PageContentFilter:DoBeforeProcessing");
+        }
 
         InputStream configStream = request
                 .getServletContext()
                 .getResourceAsStream("/WEB-INF/config/global-config.properties");
         Properties _prop = new Properties();
         _prop.load(configStream);
-        
-        //Đường dẫn tới thư mục chứa hình ảnh đồ chơi và avatar
+
+        // Đường dẫn tới thư mục chứa hình ảnh đồ chơi và avatar
         String toyImagesFolder = _prop.getProperty("ToyImagesFolder");
         String avatarImagesFolder = _prop.getProperty("AvatarImagesFolder");
         request.setAttribute("ToyImagesFolder", toyImagesFolder);
         request.setAttribute("AvatarImagesFolder", avatarImagesFolder);
-                  
-        // pass the request along the filter chain
-        chain.doFilter(request, response);
+    }
+
+    private void doAfterProcessing(ServletRequest request,
+            ServletResponse response) throws IOException, ServletException
+    {
+        if (debug)
+        {
+            log("PageContentFilter:DoAfterProcessing");
+        }
+
+        // Write code here to process the request and/or response after
+        // the rest of the filter chain is invoked.
+
+        // For example, a logging filter might log the attributes on the
+        // request object after the request has been processed.
+        /*
+         * for (Enumeration en = request.getAttributeNames();
+         * en.hasMoreElements(); ) { String name = (String)en.nextElement();
+         * Object value = request.getAttribute(name); log("attribute: " + name +
+         * "=" + value.toString());
+         * 
+         * }
+         */
+
+        // For example, a filter might append something to the response.
+        /*
+         * PrintWriter respOut = new PrintWriter(response.getWriter());
+         * respOut.println
+         * ("<P><B>This has been appended by an intrusive filter.</B>");
+         */
     }
 
     /**
-     * @see Filter#init(FilterConfig)
+     * 
+     * @param request
+     *            The servlet request we are processing
+     * @param response
+     *            The servlet response we are creating
+     * @param chain
+     *            The filter chain we are processing
+     * 
+     * @exception IOException
+     *                if an input/output error occurs
+     * @exception ServletException
+     *                if a servlet error occurs
      */
-    public void init(FilterConfig fConfig) throws ServletException
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException
     {
-        // TODO Auto-generated method stub
+
+        if (debug)
+        {
+            log("PageContentFilter:doFilter()");
+        }
+
+        doBeforeProcessing(request, response);
+
+        Throwable problem = null;
+        try
+        {
+            chain.doFilter(request, response);
+        }
+        catch (Throwable t)
+        {
+            // If an exception is thrown somewhere down the filter chain,
+            // we still want to execute our after processing, and then
+            // rethrow the problem after that.
+            problem = t;
+            t.printStackTrace();
+        }
+
+        doAfterProcessing(request, response);
+
+        // If there was a problem, we want to rethrow it if it is
+        // a known type, otherwise log it.
+        if (problem != null)
+        {
+            if (problem instanceof ServletException)
+            {
+                throw (ServletException) problem;
+            }
+            if (problem instanceof IOException)
+            {
+                throw (IOException) problem;
+            }
+            sendProcessingError(problem, response);
+        }
     }
 
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig()
+    {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     * 
+     * @param filterConfig
+     *            The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig)
+    {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy()
+    {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig)
+    {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null)
+        {
+            if (debug)
+            {
+                log("PageContentFilter:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString()
+    {
+        if (filterConfig == null)
+        {
+            return ("PageContentFilter()");
+        }
+        StringBuffer sb = new StringBuffer("PageContentFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response)
+    {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals(""))
+        {
+            try
+            {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); // NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); // NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        else
+        {
+            try
+            {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+
+    public static String getStackTrace(Throwable t)
+    {
+        String stackTrace = null;
+        try
+        {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        }
+        catch (Exception ex)
+        {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg)
+    {
+        filterConfig.getServletContext().log(msg);
+    }
 }
